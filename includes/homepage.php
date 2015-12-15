511e45ef8ccaf1dbaa4750297c1ec9f1d3972c1f
Quendoline <quendoline.jansen@gmail.com>
2015-12-15 08:04:59 +0100

    Changes hero homepage

--- includes/homepage.php
+++ includes/homepage.php
@@ -1,23 +1,33 @@
 <section class="container hero">
+    
     <section class="wrapper">
-        <div class="column-mid-left">
+
+        <h1 class="slogan"> Keep them hugging </h1>
+        <h3 class="onderzin"> Help een getraumatiseerd kind in een conflictgebied</h3>
+        <!-- <div class="column-mid-left">
+
             <img src="/hackathon/lib/images/meisjesbeer.png" alt="">
-        </div>
-        <div class="column-mid-right">
+        </div> -->
+       <!--  <div class="column-mid-right">
             <img src="/hackathon/lib/images/ouderen.jpg" alt="">
-        </div>
+        </div> -->
         <div class="clear"></div>
+
+        <p class="readmore"> Lees meer </p>
     </section>
 </section>
 <section class="container blue">
     <section class="wrapper">
         <div class="column-mid-left">
+
             <img src="/hackathon/lib/images/knuffel.png" alt="">
         </div>
         <div class="column-mid-right">
+
             <h2>dfasfdsafdsa</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus nunc mi, sed sollicitudin dolor pharetra a. Aliquam erat volutpat. In hac habitasse platea dictumst. Donec eget imperdiet est, at porta tellus. Donec non tincidunt quam. Aliquam enim felis, pretium a diam nec, tristique porttitor massa. Duis at pellentesque nulla. Curabitur nisl felis, congue a nulla nec, semper interdum neque. Phasellus interdum nulla vitae tincidunt eleifend. Vivamus viverra massa eget consectetur aliquam. Donec varius libero velit, non sodales libero semper non.</p>
         </div>
         <div class="clear"></div>
+
     </section>
 </section>
