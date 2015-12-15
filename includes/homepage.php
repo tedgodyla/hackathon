6e0a71eb7f118fea7b5683cc4a3edfbfb772eefc
tedgodyla <tedgodyla@gmail.com>
2015-12-15 13:18:40 +0100

    Changes to homepage

--- includes/homepage.php
+++ includes/homepage.php
@@ -1,16 +1,7 @@
 <section class="container hero">
-
     <section class="wrapper">
-
         <h1 class="slogan">Keep them hugging</h1>
         <h3 class="onderzin">Help een getraumatiseerd kind<br>in een conflictgebied</h3>
-        <!-- <div class="column-mid-left">
-
-            <img src="/hackathon/lib/images/meisjesbeer.png" alt="">
-        </div> -->
-       <!--  <div class="column-mid-right">
-            <img src="/hackathon/lib/images/ouderen.jpg" alt="">
-        </div> -->
         <div class="clear"></div>
     </section>
 </section>
@@ -18,11 +9,10 @@
 <section class="container white">
     <section class="wrapper">
         <div class="column-center">
-
-            <h2> TeddyPy helpt</h2>
-            <p> Samen met de TeddyPy start het kind een verwerkingsfase, zodat er later meer plek is voor educatie. In Nederland maken we onderdelen 
-                voor de TeddyPy, dit de kinderen in conflictlanden kunnen gebruiken om hun knuffel 'eigen' te maken. Door persoonlijke band met de knuffel
-                speelt TeddyPy een rol in de traumaverwerking van het kind. </p>
+            <h2> TeddyPy helpt de stille slachtoffer</h2>
+            <p>Samen met de TeddyPy start het kind een verwerkingsfase. De TeddyPy begint hier in Nederland waar de stukjes 
+                voor de TeddyPy gemaakt worden, die de kinderen in conflictlanden kunnen gebruiken om hun knuffel 'eigen' te maken. Door persoonlijke band met de knuffel
+                speelt TeddyPy een rol in de traumaverwerking van het kind. Dit maakt bij het kind plek vrij voor educatie.</p>
         <div class="clear"></div>
     </section>
 </section>
@@ -42,6 +32,16 @@
             <h2>Wat is Teddypy?</h2>
             <p>Door Teddypy kunnen kinderen hun opgelopen trauma verwerken. Hierdoor kunnen zij genieten van het naar school gaan en nieuwe dingen leren. Op deze manier kunnen zij veilig aan hun toekomst werken. Teddypy is te personaliseren door het kind. Soorten kleuren, texturen en structuren zorgen voor veel variatie tussen de Teddypy’s. Zo draagt de Teddypy ook bij aan de toekomst dat ze er mee verder kunnen. Wil je ook helpen? Kijk wat jou school of tehuis kan doen. Zorg dat meer mensen zich hiervoor gaan inzetten.</p>
         </div>
+        <div class="clear"></div>
+    </section>
+</section>
+
+<section class="container grey">
+    <section class="wrapper">
+        <div class="column-center">
+            <h2>Hulp van overal</h2>
+            <p>TeddyPy stukjes worden niet alleen niet Nederlandland gemaakt, maar overal ter wereld. Met zijn alle helpen wij het kind weer kind te zijn.</p><br><br>
+            <img src="/hackathon/lib/images/kaart.png" width="500" alt="">
         <div class="clear"></div>
     </section>
 </section>
